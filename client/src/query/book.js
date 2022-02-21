6319dfd3a798ff666eac52ad7b86ad7ab5b71c9f
Sveta <sweta4ca@mail.ru>
2022-02-21 13:47:04 +0700

    fixed Books.js component and query book.js

--- client/src/query/book.js
+++ client/src/query/book.js
@@ -3,15 +3,15 @@ import {gql} from '@apollo/client'
 export const GET_ALL_BOOKS = gql(`
     query getAllBooks($itemPerPage: Int, $offset: Int, $limitValue: Int, $shouldReset: Boolean) {
         getAllBooks(itemPerPage: $itemPerPage, offset: $offset, limitValue: $limitValue, shouldReset: $shouldReset) {
-            currentID, title, author{currentID, name}
+            title, author{name}
         }
     }
-`) // в видосе нет скобочек
+`)
 
 export const GET_ONE_BOOK = gql(`
     query getBook($id: ID) {
         getBook(id: $id) {
-            currentID, title, author{currentID, name}
+            title, author{name}
         }
     }
-`) // в видосе нет скобочек
+`)
